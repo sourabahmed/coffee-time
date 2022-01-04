2784f9833831032c731ac101f731ab7e8c21d5d3
unknown <sourabahmed546@gmail.com>
2022-01-05 00:13:53 +0600

    review-error-done

--- src/pages/Home/Reviews/Reviews.js
+++ src/pages/Home/Reviews/Reviews.js
@@ -7,7 +7,7 @@ const Reviews = () => {
     const [reviews, setReviews] = useState([])
 
     useEffect(() => {
-        fetch('review.json')
+        fetch('http://localhost:5000/reviews')
             .then(res => res.json())
             .then(data => setReviews(data));
     }, [reviews])
@@ -19,15 +19,15 @@ const Reviews = () => {
                     reviews.map(review =>
                         <Carousel.Item className="d-flex justify-content-center banner" key={review._id}>
                             <div className="review py-4 carousel-width front-bg">
-                                <img className="my-2 rounded-circle" src={review.img} alt="" />
+                                <img className="my-2 rounded-circle" src={review.image} alt="" />
                                 <h3 className="pt-3 pb-0 mb-0 fw-bold">{review.name}</h3>
-                                <h5 className="pt-0 pb-3">{review.designation}</h5>
+                                <h5 className="pt-0 pb-3">{review.title}</h5>
                                 <div className="mt-1 mb-3">
                                     <Rating
-                                        emptySymbol="far fa-star fa-1x"
-                                        fullSymbol="fas fa-star fa-1x"
-                                        initialRating={review.rating}
                                         readonly
+                                        initialRating={review.rating}
+                                        emptySymbol="far fa-star text-warning"
+                                        fullSymbol="fas fa-star text-warning"
                                     />
                                 </div>
                                 <p className="fst-italic px-3 fw-bold"> {review.description}</p>
